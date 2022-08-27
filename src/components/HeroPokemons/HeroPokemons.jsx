cbaf6e3467de9b8bea3c099972892bd95f7cee9a
chocopros <jesuschock93@gmail.com>
2022-08-27 16:24:07 -0400

    start-424

--- src/components/HeroPokemons/HeroPokemons.jsx
+++ src/components/HeroPokemons/HeroPokemons.jsx
@@ -4,19 +4,32 @@ import './Heropoke.css'
 const HeroPokemons = () => {
   return (
     <section className='Hero-Pokemons'>
-      <div>
-        <img src="src\assets\img\LOGO.svg" alt="" />
+
+      <div className="hero-body">
+        <div>
+          <img src="src\assets\img\LOGO.svg" alt="" />
+        </div>
+        <div className='title-ppal'>
+          <h2>¡Hola entrenador!</h2>
+          <h3>Para poder comenzar, dame tu nombre</h3>
+        </div>
+        <div className="input-trainer-pokemons">
+          <input placeholder='Tu Nombre...' type="text" />
+          <button>Comenzar</button>
+        </div>
       </div>
-      <div className='title-ppal'>
-        <h2>¡Hola entrenador!</h2>
-        <h3>Para poder comenzar, dame tu nombre</h3>
-      </div>
-      <div className="input-trainer-pokemons">
-        <input type="text" />
-        <button>Comenzar</button>
-      </div>
+   
+
+      <footer className='footerHero'>
+        <div className="img-button-poke">
+          <img className='back-button' src="src\assets\img\img-hero\Ellipse 3.png" alt="" />
+          <img className='up-button' src="src\assets\img\img-hero\Ellipse 4.png" alt="" />
+        </div>
+        <div className='back-black'></div>
+      </footer>
       
     </section>
+    
   )
 }
 
