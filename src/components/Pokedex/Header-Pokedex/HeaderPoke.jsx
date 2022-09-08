1c94f75094d54f3651215f0ab87376d3956c3e33
Jesuschock <jesuschock93@gmail.com>
2022-09-08 17:09:42 -0400

    img

--- src/components/Pokedex/Header-Pokedex/HeaderPoke.jsx
+++ src/components/Pokedex/Header-Pokedex/HeaderPoke.jsx
@@ -9,8 +9,8 @@ const HeaderPoke = () => {
           </div>
           <div className='black-line'>
               <div className="closet-ball">
-                  <img src="https://eastus1-mediap.svc.ms/transform/thumbnail?provider=spo&inputFormat=png&cs=fFNQTw&docid=https%3A%2F%2Fdmonroy-my.sharepoint.com%3A443%2F_api%2Fv2.0%2Fdrives%2Fb!WhY8MwbTfkKcv-N9sOaGBV8BdC9VgpRKv6m0ZMYUyoTj6eMAQNwlSoQSUjM3ctPo%2Fitems%2F01RRHQARZWELHYDWRTPREIE6UYQ42GRH25%3Fversion%3DPublished&access_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJub25lIn0.eyJhdWQiOiIwMDAwMDAwMy0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAvZG1vbnJveS1teS5zaGFyZXBvaW50LmNvbUBiYzkzZDJlNi0yNjcwLTRiNDQtYjdlOC04NDAzNDY5MDU3NjYiLCJpc3MiOiIwMDAwMDAwMy0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAiLCJuYmYiOiIxNjYxOTkwNDAwIiwiZXhwIjoiMTY2MjAxMjAwMCIsImVuZHBvaW50dXJsIjoiYUF2cHhhbnBRMmxSeEJySFlPclluK2h3T1dVTVNoZ2k1ajUxa2JvWUt3UT0iLCJlbmRwb2ludHVybExlbmd0aCI6IjExNyIsImlzbG9vcGJhY2siOiJUcnVlIiwidmVyIjoiaGFzaGVkcHJvb2Z0b2tlbiIsInNpdGVpZCI6Ik16TXpZekUyTldFdFpETXdOaTAwTWpkbExUbGpZbVl0WlRNM1pHSXdaVFk0TmpBMSIsInNpZ25pbl9zdGF0ZSI6IltcImttc2lcIl0iLCJuYW1laWQiOiIwIy5mfG1lbWJlcnNoaXB8dGVjaG5vbG9neUBqZG1ncm91cGNvbXBhbnkuY29tIiwibmlpIjoibWljcm9zb2Z0LnNoYXJlcG9pbnQiLCJpc3VzZXIiOiJ0cnVlIiwiY2FjaGVrZXkiOiIwaC5mfG1lbWJlcnNoaXB8MTAwMzIwMDBkNDk2NjhkN0BsaXZlLmNvbSIsInNpZCI6Ijg3M2FhNDMyLTNmOGUtNDI2ZS1iZmVmLTE3ODlkYzA5MTA4ZSIsInR0IjoiMCIsInVzZVBlcnNpc3RlbnRDb29raWUiOiIzIiwiaXBhZGRyIjoiMjAwLjQ0LjI0MC41OCJ9.MXE5MVFFU00xbE9kQ3hibVdHZHZBOC9yMlZhbzBNOGpxR1htd0xrdkZhZz0&cTag=%22c%3A%7B81CF2236-33DA-487C-827A-988734689F5D%7D%2C1%22&encodeFailures=1&width=117&height=117&srcWidth=117&srcHeight=117" alt="" />
-                  <img className='ball-point' src="https://i.ibb.co/gRw02Jv/Ellipse-4.png" alt="" />
+                  <img src="src/assets/img/img-pokedex/Ellipse 3.png" alt="" />
+                  <img className='ball-point' src="src/assets/img/img-pokedex/Ellipse 4.png" alt="" />
               </div>
           </div>
       </header>
